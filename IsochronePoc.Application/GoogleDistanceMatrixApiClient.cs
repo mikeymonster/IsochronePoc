43e38d566ed69b2df6a685d4ff050242fd4bd31b
Mike Wild <wildconsultingltd@googlemail.com>
2019-09-06 10:24:05 +0100

    Write out results

--- IsochronePoc.Application/GoogleDistanceMatrixApiClient.cs
+++ IsochronePoc.Application/GoogleDistanceMatrixApiClient.cs
@@ -42,12 +42,14 @@ namespace IsochronePoc.Application
 
             var stopwatch = Stopwatch.StartNew();
 
-            Parallel.ForEach(batches, async batch =>
+            Parallel.ForEach(batches, batch =>
             {
                 var (key, value) = batch;
                 Console.WriteLine($"Processing batch {key} of size {value.Count}");
 
-                var response = await SearchBatch(origin, value);
+                //Don't use async on body of Parallel.ForEach and wait here - it doesn't block
+                //var response = await SearchBatch(origin, value);
+                var response = SearchBatch(origin, value).GetAwaiter().GetResult();
                 if (response != null)
                 {
                     lock (_listLocker)
@@ -60,6 +62,12 @@ namespace IsochronePoc.Application
             stopwatch.Stop();
             Console.WriteLine($"Have {results.Count} results from {batches.Count} batches of {batchSize} in {stopwatch.ElapsedMilliseconds:#,###}ms");
 
+            foreach (var x in results)
+            {
+                Console.WriteLine($"{x.Rows.Length} - {x.DestinationAddresses.Length}");
+            }
+            
+            //results = results.Where(x => x.DestinationAddresses.Length < 100).ToList();
             await BuildGoogleResults(results);
         }
 
@@ -94,15 +102,9 @@ namespace IsochronePoc.Application
                     Console.WriteLine($"  Row has {row.Elements.Length} elements");
                     foreach (var element in row.Elements)
                     {
-                        Console.WriteLine($"    {element.Status}, {element.Distance}, {element.Duration}");
+                        Console.WriteLine($"    {element.Status}, {element.Distance.Text}: {element.Distance.Value}, {element.Duration.Text}: {element.Duration.Value}");
                     }
                 }
-
-                foreach (var row in item.Rows)
-                {
-                    
-                }
-                
             }
         }
 
@@ -123,68 +125,77 @@ namespace IsochronePoc.Application
 
         private async Task<GoogleDistanceMatrixResponse> SearchBatch(Venue origin, IList<Venue> venues)
         {
-            //https://developers.google.com/maps/documentation/distance-matrix/intro
-
-            //Call:
-            //http://maps.googleapis.com/maps/api/distancematrix/outputFormat?parameters
-            //var uri = "distancematrix";
-            //NOTE: Assumes api url already has ending /
-            var uriBuilder = new StringBuilder($@"{_configuration.GoogleMapsApiBaseUrl}distancematrix/json?");
-
-            uriBuilder.Append($"origins={origin.Latitude}%2C{origin.Longitude}");
-            uriBuilder.Append("&destinations=");
-
-            for (int i = 0; i < venues.Count; i++)
+            try
             {
-                var venue = venues[i];
+                //https://developers.google.com/maps/documentation/distance-matrix/intro
 
-                if (i > 0)
+                //Call:
+                //http://maps.googleapis.com/maps/api/distancematrix/outputFormat?parameters
+                //var uri = "distancematrix";
+                //NOTE: Assumes api url already has ending /
+                var uriBuilder = new StringBuilder($@"{_configuration.GoogleMapsApiBaseUrl}distancematrix/json?");
+
+                uriBuilder.Append($"origins={origin.Latitude}%2C{origin.Longitude}");
+                uriBuilder.Append("&destinations=");
+
+                for (int i = 0; i < venues.Count; i++)
                 {
-                    uriBuilder.Append($"%7C");
+                    var venue = venues[i];
+
+                    if (i > 0)
+                    {
+                        uriBuilder.Append($"%7C");
+                    }
+                    //uriBuilder.Append($"{venue.Latitude}%2C{venue.Longitude}");
+                    //uriBuilder.Append($"{WebUtility.UrlEncode(venue.Postcode)}");
+                    uriBuilder.Append($"{venue.Postcode.Replace(" ", "")}");
                 }
-                //uriBuilder.Append($"{venue.Latitude}%2C{venue.Longitude}");
-                //uriBuilder.Append($"{WebUtility.UrlEncode(venue.Postcode)}");
-                uriBuilder.Append($"{venue.Postcode.Replace(" ", "")}");
-            }
 
-            uriBuilder.Append($"&key={_configuration.GoogleMapsApiKey}");
+                uriBuilder.Append($"&key={_configuration.GoogleMapsApiKey}");
 
-            Console.WriteLine("Calling google distance matrix api with uri");
-            Console.WriteLine(uriBuilder);
+                Console.WriteLine("Calling google distance matrix api with uri");
+                Console.WriteLine(uriBuilder);
 
-            var stopwatch = Stopwatch.StartNew();
+                var stopwatch = Stopwatch.StartNew();
 
-            var response = await _httpClient.GetAsync(uriBuilder.ToString());
+                var response = await _httpClient.GetAsync(uriBuilder.ToString());
 
-            stopwatch.Stop();
+                stopwatch.Stop();
 
-            Console.WriteLine($"Received {response.StatusCode} in {stopwatch.ElapsedMilliseconds:#,###}ms");
+                Console.WriteLine($"Received {response.StatusCode} in {stopwatch.ElapsedMilliseconds:#,###}ms");
 
-            response.EnsureSuccessStatusCode();
+                response.EnsureSuccessStatusCode();
 
-            //var content = await response.Content.ReadAsStringAsync();
-            //Console.WriteLine(content);
-            //Debug.WriteLine(content);
+                //var content = await response.Content.ReadAsStringAsync();
+                //Console.WriteLine(content);
+                //Debug.WriteLine(content);
 
-            var settings = new JsonSerializerSettings
-            {
-                MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
-                DateParseHandling = DateParseHandling.None,
-                Converters =
+                var settings = new JsonSerializerSettings
+                {
+                    MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+                    DateParseHandling = DateParseHandling.None,
+                    Converters =
                 {
                     new IsoDateTimeConverter {DateTimeStyles = DateTimeStyles.AssumeUniversal}
                 }
-            };
+                };
 
-            using (var stream = await response.Content.ReadAsStreamAsync())
-            using (var reader = new StreamReader(stream))
-            using (var jsonReader = new JsonTextReader(reader))
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                using (var reader = new StreamReader(stream))
+                using (var jsonReader = new JsonTextReader(reader))
+                {
+                    var serializer = new JsonSerializer();
+                    var result = (GoogleDistanceMatrixResponse)serializer.Deserialize(jsonReader,
+                            typeof(GoogleDistanceMatrixResponse));
+
+                    return result;
+                }
+
+            }
+            catch (Exception ex)
             {
-                var serializer = new JsonSerializer();
-                var result = (GoogleDistanceMatrixResponse)serializer.Deserialize(jsonReader,
-                        typeof(GoogleDistanceMatrixResponse));
-
-                return result;
+                Console.WriteLine($"Failure calling google api - {ex}");
+                throw;
             }
         }
     }
